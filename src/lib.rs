9a8b82b446886adb1e97b3b6eaedbfb81c6f057c
Jake McGinty <me@jake.su>
2019-12-05 19:21:06 +0900

    improve documentation in FFI bindings

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 
 use std::{error, fmt, sync::Arc};
 pub use webrtc_audio_processing_sys as ffi;
-use ffi::*;
+pub use ffi::{InitializationConfig, EchoCancellation, EchoCancellation_SuppressionLevel, Config, NUM_SAMPLES_PER_FRAME};
 
 /// Represents an error inside webrtc::AudioProcessing.
 /// See the documentation of [`webrtc::AudioProcessing::Error`](https://cgit.freedesktop.org/pulseaudio/webrtc-audio-processing/tree/webrtc/modules/audio_processing/include/audio_processing.h?id=9def8cf10d3c97640d32f1328535e881288f700f)
--- webrtc-audio-processing-sys/src/wrapper.hpp
+++ webrtc-audio-processing-sys/src/wrapper.hpp
@@ -7,64 +7,81 @@
 namespace webrtc_audio_processing {
 
 // AudioProcessing accepts only one of 48000, 32000, 16000, and 8000 hz.
-// Tonari only cares about 48000.
+// TODO: support multiple sample rates.
 const int SAMPLE_RATE_HZ = 48000;
 
 // AudioProcessing expects each frame to be of fixed 10 ms.
 const int FRAME_MS = 10;
 
+/// <div rustbindgen>The number of expected samples per frame.</div>
 const int NUM_SAMPLES_PER_FRAME = SAMPLE_RATE_HZ * FRAME_MS / 1000;
 
 struct AudioProcessing;
 
+/// <div rustbindgen>A configuration used only when initializing a Processor.</div>
 struct InitializationConfig {
   int num_capture_channels;
   int num_render_channels;
 
-  // TODO(ryo): Investigate how it's different from the default gain control and
-  // the effect of using the two at the same time.
+  // TODO: Investigate how it's different from the default gain control and the effect of using the two at the same time.
   bool enable_experimental_agc;
 
   bool enable_intelligibility_enhancer;
 };
 
+/// <div rustbindgen>Echo cancellation configuration.</div>
 struct EchoCancellation {
+  /// <div rustbindgen>Whether to use echo cancellation.</div>
   bool enable;
 
+  /// <div rustbindgen>A level of echo suppression.</div>
   enum SuppressionLevel {
       LOW,
       MODERATE,
       HIGH,
   };
 
-  // Determines the aggressiveness of the suppressor. A higher level trades off
-  // double-talk performance for increased echo suppression.
+  /// <div rustbindgen>
+  /// Determines the aggressiveness of the suppressor. A higher level trades off
+  /// double-talk performance for increased echo suppression.
+  /// </div>
   SuppressionLevel suppression_level;
 };
 
+/// <div rustbindgen>Gain control configuration.</div>
 struct GainControl {
+  /// <div rustbindgen>Whether to use gain control.</div>
   bool enable;
 
-  // Sets the target peak level (or envelope) of the AGC in dBFs (decibels from
-  // digital full-scale). The convention is to use positive values.
-  // For instance, passing in a value of 3 corresponds to -3 dBFs, or a target
-  // level 3 dB below full-scale. Limited to [0, 31].
+  /// <div rustbindgen>
+  /// Sets the target peak level (or envelope) of the AGC in dBFs (decibels from
+  /// digital full-scale). The convention is to use positive values.
+  /// For instance, passing in a value of 3 corresponds to -3 dBFs, or a target
+  /// level 3 dB below full-scale. Limited to [0, 31].
+  /// </div>
   int target_level_dbfs;
 
-  // Sets the maximum gain the digital compression stage may apply, in dB. A
-  // higher number corresponds to greater compression, while a value of 0 will
-  // leave the signal uncompressed. Limited to [0, 90].
+  /// <div rustbindgen>
+  /// Sets the maximum gain the digital compression stage may apply, in dB. A
+  /// higher number corresponds to greater compression, while a value of 0 will
+  /// leave the signal uncompressed. Limited to [0, 90].
+  /// </div>
   int compression_gain_db;
 
-  // When enabled, the compression stage will hard limit the signal to the
-  // target level. Otherwise, the signal will be compressed but not limited
-  // above the target level.
+  /// <div rustbindgen>
+  /// When enabled, the compression stage will hard limit the signal to the
+  /// target level. Otherwise, the signal will be compressed but not limited
+  /// above the target level.
+  /// </div>
   bool enable_limiter;
 };
 
+/// <div rustbindgen>Noise suppression configuration.</div>
 struct NoiseSuppression {
+  /// <div rustbindgen>Whether to use noise supression.</div>
   bool enable;
 
+  /// <div rustbindgen>A level of noise suppression.</div>
   enum SuppressionLevel {
       LOW,
       MODERATE,
@@ -72,14 +89,19 @@ struct NoiseSuppression {
       VERY_HIGH,
   };
 
-  // Determines the aggressiveness of the suppression. Increasing the level will
-  // reduce the noise level at the expense of a higher speech distortion.
+  /// <div rustbindgen>
+  /// Determines the aggressiveness of the suppression. Increasing the level will
+  /// reduce the noise level at the expense of a higher speech distortion.
+  /// </div>
   SuppressionLevel suppression_level;
 };
 
+/// <div rustbindgen>Voice detection configuration.</div>
 struct VoiceDetection {
+  /// <div rustbindgen>Whether to use voice detection.</div>
   bool enable;
 
+  /// <div rustbindgen>The sensitivity of the noise detector.</div>
   enum DetectionLikelihood {
       VERY_LOW,
       LOW,
@@ -87,34 +109,45 @@ struct VoiceDetection {
       HIGH,
   };
 
-  // Specifies the likelihood that a frame will be declared to contain voice. A
-  // higher value makes it more likely that speech will not be clipped, at the
-  // expense of more noise being detected as voice.
+  /// <div rustbindgen>
+  /// Specifies the likelihood that a frame will be declared to contain voice. A
+  /// higher value makes it more likely that speech will not be clipped, at the
+  /// expense of more noise being detected as voice.
+  /// </div>
   DetectionLikelihood detection_likelihood;
 };
 
+/// <div rustbindgen>Config that can be used mid-processing.</div>
 struct Config {
   EchoCancellation echo_cancellation;
   GainControl gain_control;
   NoiseSuppression noise_suppression;
   VoiceDetection voice_detection;
 
-  // Use to enable the extended filter mode in the AEC, along with robustness
-  // measures around the reported system delays. It comes with a significant
-  // increase in AEC complexity, but is much more robust to unreliable reported
-  // delays.
+  /// <div rustbindgen>
+  /// Use to enable the extended filter mode in the AEC, along with robustness
+  /// measures around the reported system delays. It comes with a significant
+  /// increase in AEC complexity, but is much more robust to unreliable reported
+  /// delays.
+  /// </div>
   bool enable_extended_filter;
 
-  // Enables delay-agnostic echo cancellation. This feature relies on internally
-  // estimated delays between the process and reverse streams, thus not relying
-  // on reported system delays.
+  /// <div rustbindgen>
+  /// Enables delay-agnostic echo cancellation. This feature relies on internally
+  /// estimated delays between the process and reverse streams, thus not relying
+  /// on reported system delays.
+  /// </div>
   bool enable_delay_agnostic;
 
-  // Use to enable experimental transient noise suppression.
+  /// <div rustbindgen>
+  /// Use to enable experimental transient noise suppression.
+  /// </div>
   bool enable_transient_suppressor;
 
-  // Use to enable a filtering component which removes DC offset and
-  // low-frequency noise.
+  /// <div rustbindgen>
+  /// Use to enable a filtering component which removes DC offset and
+  /// low-frequency noise.
+  /// </div>
   bool enable_high_pass_filter;
 };
 
@@ -133,48 +166,71 @@ struct OptionalBool {
   bool value = false;
 };
 
+/// <div rustbindgen>Statistics about the processor state.</div>
 struct Stats {
-  // True if voice is detected in the current frame.
+  /// <div rustbindgen>
+  /// True if voice is detected in the current frame.
+  /// </div>
   OptionalBool has_voice;
 
-  // False if the current frame almost certainly contains no echo and true if it
-  // _might_ contain echo.
+  /// <div rustbindgen>
+  /// False if the current frame almost certainly contains no echo and true if it
+  /// _might_ contain echo.
+  /// </div>
   OptionalBool has_echo;
 
-  // Root mean square (RMS) level in dBFs (decibels from digital full-scale), or
-  // alternately dBov. It is computed over all primary stream frames since the
-  // last call to |get_stats()|. The returned value is constrained to [-127, 0],
-  // where -127 indicates muted.
+  /// <div rustbindgen>
+  /// Root mean square (RMS) level in dBFs (decibels from digital full-scale), or
+  /// alternately dBov. It is computed over all primary stream frames since the
+  /// last call to |get_stats()|. The returned value is constrained to [-127, 0],
+  /// where -127 indicates muted.
+  /// </div>
   OptionalInt rms_dbfs;
 
-  // Prior speech probability of the current frame averaged over output
-  // channels, internally computed by noise suppressor.
+  /// <div rustbindgen>
+  /// Prior speech probability of the current frame averaged over output
+  /// channels, internally computed by noise suppressor.
+  /// </div>
   OptionalDouble speech_probability;
 
-  // RERL = ERL + ERLE
+  /// <div rustbindgen>
+  /// RERL = ERL + ERLE
+  /// </div>
   OptionalDouble residual_echo_return_loss;
 
-  // ERL = 10log_10(P_far / P_echo)
+  /// <div rustbindgen>
+  /// ERL = 10log_10(P_far / P_echo)
+  /// </div>
   OptionalDouble echo_return_loss;
 
-  // ERLE = 10log_10(P_echo / P_out)
+  /// <div rustbindgen>
+  /// ERLE = 10log_10(P_echo / P_out)
+  /// </div>
   OptionalDouble echo_return_loss_enhancement;
 
-  // (Pre non-linear processing suppression) A_NLP = 10log_10(P_echo / P_a)
+  /// <div rustbindgen>
+  /// (Pre non-linear processing suppression) A_NLP = 10log_10(P_echo / P_a)
+  /// </div>
   OptionalDouble a_nlp;
 
-  // Median of the measured delay in ms. The values are aggregated until the
-  // first call to |get_stats()| and afterwards aggregated and updated every
-  // second.
+  /// <div rustbindgen>
+  /// Median of the measured delay in ms. The values are aggregated until the
+  /// first call to |get_stats()| and afterwards aggregated and updated every
+  /// second.
+  /// </div>
   OptionalInt delay_median_ms;
 
-  // Standard deviation of the measured delay in ms. The values are aggregated
-  // until the first call to |get_stats()| and afterwards aggregated and updated
-  // every second.
+  /// <div rustbindgen>
+  /// Standard deviation of the measured delay in ms. The values are aggregated
+  /// until the first call to |get_stats()| and afterwards aggregated and updated
+  /// every second.
+  /// </div>
   OptionalInt delay_standard_deviation_ms;
 
-  // The fraction of delay estimates that can make the echo cancellation perform
-  // poorly.
+  /// <div rustbindgen>
+  /// The fraction of delay estimates that can make the echo cancellation perform
+  /// poorly.
+  /// </div>
   OptionalDouble delay_fraction_poor_delays;
 };
 
