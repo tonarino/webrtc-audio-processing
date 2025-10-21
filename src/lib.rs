0921d87a8f156faa58846a82bafc0c5efbb03a3a
Marek Bernat <mbernat@users.noreply.github.com>
2025-10-21 12:35:15 +0200

    Fix a typo

--- src/lib.rs
+++ src/lib.rs
@@ -168,11 +168,11 @@ impl Processor {
     }
 }
 
-/// `AudioProcessor` provides an access to webrtc's audio processing e.g. echo
-/// cancellation and automatic gain control.
-/// This is a low level API that may require wrapping in your Arc to be shared between threads, depending on
-/// the use case. See [`Processor`] for a simple wrapper around this API that enables sharing
-/// the processor between threads.
+/// `AudioProcessing` provides an access to webrtc's audio processing e.g. echo cancellation and
+/// automatic gain control.
+/// This is a low level API that may require wrapping in your Arc to be shared between threads,
+/// depending on the use case. See [`Processor`] for a simple wrapper around this API that enables
+/// sharing the processor between threads.
 pub struct AudioProcessing {
     inner: *mut ffi::AudioProcessing,
 }
