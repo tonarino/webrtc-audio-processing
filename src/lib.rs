f8f2f44fcf82a42772a50fd814fcf085b9f27e60
Marek Bernat <mbernat@users.noreply.github.com>
2025-10-22 18:08:00 +0900

    Apply suggestions from code review
    Wording improvements
    
    Co-authored-by: Brian Schwind <brianmschwind@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -168,9 +168,9 @@ impl Processor {
     }
 }
 
-/// `AudioProcessing` provides an access to webrtc's audio processing e.g. echo cancellation and
+/// `AudioProcessing` provides access to webrtc's audio processing e.g. echo cancellation and
 /// automatic gain control.
-/// This is a low level API that may require wrapping in your Arc to be shared between threads,
+/// This is a low level API that may require wrapping in an Arc to be shared between threads,
 /// depending on the use case. See [`Processor`] for a simple wrapper around this API that enables
 /// sharing the processor between threads.
 pub struct AudioProcessing {
